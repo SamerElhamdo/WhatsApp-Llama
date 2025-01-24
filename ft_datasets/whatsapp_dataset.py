6030a9e484ada56e2646cabcb9e68b3ccc36bdf3
SamerElhamdo <35710586+SamerElhamdo@users.noreply.github.com>
2025-01-24 21:56:37 +0300

    Update whatsapp_dataset.py
    set my data

--- ft_datasets/whatsapp_dataset.py
+++ ft_datasets/whatsapp_dataset.py
@@ -2,10 +2,10 @@ import datasets
 from .utils import Concatenator
 
 def get_preprocessed_whatsapp(dataset_config, tokenizer, split):
-    dataset = datasets.load_dataset('csv', data_files='FinalDataset.csv')[split]
+    dataset = datasets.load_dataset('csv', data_files='/content/drive/MyDrive/github/FinalDataset.csv')[split]
 
     prompt = (
-        f"Reply to the following messages as the user Advaith. Provide just one reply, do not continue the conversation.\n{{context}}\n---\Advaith:\n{{reply}}{{eos_token}}"
+        f"Reply to the following messages as the user samer. Provide just one reply, do not continue the conversation.\n{{context}}\n---\samer:\n{{reply}}{{eos_token}}"
     )
 
     def apply_prompt_template(sample):
